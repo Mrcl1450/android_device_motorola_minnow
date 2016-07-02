f7d417ac1c340a4cbd5ec301286155a93fd55bdd
Marcelo Reis <marcelo-fil@outlook.com>
2016-07-02 16:17:28 +0100

    Add wpa supplicant config
    Also temporary dummy files while I don't have a system dump from latest
    release

--- device.mk
+++ device.mk
@@ -31,3 +31,24 @@ PRODUCT_COPY_FILES += \
 	#$(LOCAL_PATH)/rootdir/data/etc/wearable_hardware_unavailable.xml:system/etc/permissions/wearable_hardware_unavailable.xml \
 	#$(LOCAL_PATH)/rootdir/data/etc/watch_features.xml:system/etc/permissions/watch_features.xml \
 
+# Device specific init scripts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/fstab.minnow:root/fstab.minnow \
+    $(LOCAL_PATH)/rootdir/init.minnow.bt.rc:root/init.minnow.bt.rc \
+    $(LOCAL_PATH)/rootdir/init.minnow.cust.rc:root/init.minnow.cust.rc \
+    $(LOCAL_PATH)/rootdir/init.minnow.rc:root/init.minnow.rc \
+    $(LOCAL_PATH)/rootdir/init.minnow.usb.rc:root/init.minnow.usb.rc \
+    $(LOCAL_PATH)/rootdir/ueventd.minnow.rc:root/ueventd.minnow.rc
+
+# WPA supplicant config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/system/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+    $(LOCAL_PATH)/rootdir/system/etc/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
+
+# Media
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/system/etc/media_codecs.xml:system/etc/media_codecs.xml
+
+# Audio config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/system/etc/audio_policy.conf:system/etc/audio_policy.conf
